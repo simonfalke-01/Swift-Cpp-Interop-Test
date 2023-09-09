940a51bee0e6ffc129f81f57807a0b2247013663
simonfalke-01 <ziqibrandonli@gmail.com>
2023-09-09 18:03:53 +0800

    Initial Commit

---
+++ Swift-Cpp-Interop-Test/Swift_Cpp_Interop_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Cpp_Interop_TestApp.swift
+//  Swift-Cpp-Interop-Test
+//
+//  Created by Brandon Li on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Cpp_Interop_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
